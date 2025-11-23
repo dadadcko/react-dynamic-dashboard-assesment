168f16f05c7204996e5431d66429f32658202812
Daniel Slanina <dano.slanina@gmail.com>
2025-11-23 13:13:31 +0100

    feat: add loader for higher order component WidgetWithRemoteDataComponent

--- src/widgets/core/widgetWithRemoteData.component.tsx
+++ src/widgets/core/widgetWithRemoteData.component.tsx
@@ -1,7 +1,8 @@
-import { createElement, type FunctionComponent } from "react";
+import { createElement, type FunctionComponent, type ReactNode, useRef } from "react";
 import type { WidgetConfigWithRemoteData } from "@/widgets/core/widget.type.ts";
 import { useFetch } from "@mantine/hooks";
 import { getByPath } from "@/utils/path.utils.ts";
+import { Flex, Loader, Text } from "@mantine/core";
 
 const dataKeyNotValidError = (dataKey: string) =>
   new Error(`Could not extract data using dataKey: ${dataKey ? dataKey : "<empty>"}`);
@@ -14,9 +15,17 @@ export interface WidgetWithRemoteDataProps<T extends WidgetConfigWithRemoteData,
   data: D;
 }
 
+/**
+ * Props that the optional loader component will receive
+ */
+export interface WidgetWithRemoteDataLoaderProps<T extends WidgetConfigWithRemoteData> {
+  widget: T;
+}
+
 export interface WidgetWithRemoteDataComponentProps<T extends WidgetConfigWithRemoteData, D> {
   widget: T;
   child: FunctionComponent<WidgetWithRemoteDataProps<T, D>>;
+  loader?: FunctionComponent<WidgetWithRemoteDataLoaderProps<T>>;
 }
 
 /**
@@ -30,20 +39,31 @@ export interface WidgetWithRemoteDataComponentProps<T extends WidgetConfigWithRe
 export function WidgetWithRemoteDataComponent<T extends WidgetConfigWithRemoteData, D>({
   widget,
   child,
+  loader = DefaultWidgetWithRemoteDataLoaderComponent,
 }: WidgetWithRemoteDataComponentProps<T, D>) {
   if (!widget.dataUrl) {
     throw new Error("dataUrl is missing for this widget");
   }
 
   const { data: rawData, error, loading } = useFetch(widget.dataUrl);
+  const loaderComponentRef = useRef<ReactNode>(null);
 
   // If we have an error, throw it to be caught by a widget error boundary
   if (error) {
     throw error;
   }
 
-  // Either first render or still loading
-  if (loading || !rawData) {
+  // still loading - display loader, if provided
+  if (loading) {
+    // avoid creating new loader component on each render
+    return loader ? (loaderComponentRef.current ??= createElement(loader, { widget })) : null;
+  }
+
+  // clear loader ref once loading is done
+  loaderComponentRef.current = null;
+
+  // First render - no error, no loading and no data
+  if (!rawData) {
     return null;
   }
 
@@ -56,3 +76,13 @@ export function WidgetWithRemoteDataComponent<T extends WidgetConfigWithRemoteDa
 
   return createElement(child, { widget, data });
 }
+
+// Default loader component when loading remote data, when none is provided
+const DefaultWidgetWithRemoteDataLoaderComponent: FunctionComponent = () => {
+  return (
+    <Flex direction="column" align="center">
+      <Loader type="bars" aria-hidden="true" color="dimmed" />
+      <Text c="dimmed">Loading data...</Text>
+    </Flex>
+  );
+};
